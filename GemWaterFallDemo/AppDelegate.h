cd7f30860b8a8e92fe48ab16240436e6ce5b36aa
GemShi <gemshi@GemShideMacBook-Pro.local>
2017-02-21 22:45:09 +0800

    Initial Commit

---
+++ GemWaterFallDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GemWaterFallDemo
+//
+//  Created by GemShi on 2017/2/21.
+//  Copyright © 2017年 GemShi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
